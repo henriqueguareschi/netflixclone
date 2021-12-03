48b0bc6634fcac1840d7ec6b8c5a024bf1863026
Henrique Guareschi <81583752+henriqueguareschi@users.noreply.github.com>
2021-12-02 22:25:37 -0300

    2225

---
+++ netflixclone/src/App.js
@@ -0,0 +1,47 @@
+/* eslint-disable no-unused-vars */
+import React, { useEffect, useState } from 'react';
+import './App.css';
+import Tmdb from './Tmdb';
+import FeaturedMovie from './components/FeaturedMovie';
+
+import MovieRow from './components/MovieRow';
+
+const App = () =>  {
+
+  const [movieList, setMovieList] = useState([]);
+  const [featuredData, setFeaturedData] = useState(null);
+
+  useEffect(() =>{
+    const loadAll = async () => {
+      //Pegando a lista total
+      let list = await Tmdb.getHomeList();
+      setMovieList(list);
+
+      //Pegando o Featured / Filme em destaque
+      let originals = list.filter(i=>i.slug === 'originals');
+      let randomChosen = Math.floor(Math.random() * (originals[0].items.results.length -1));
+      let chosen = originals[0].items.results[randomChosen];
+      let chosenInfo = await Tmdb.getMovieInfo(chosen.id, 'tv');
+      setFeaturedData(chosenInfo);
+    }
+
+    loadAll();
+  }, []);
+
+  return (
+    <div className="page">
+
+      {featuredData &&
+        <FeaturedMovie item={featuredData} />
+      }
+
+        <section className="lists">
+          {movieList.map((item, key)=> (
+            <MovieRow key={key} title={item.title} items={item.items}/>
+          ))}
+        </section>
+    </div>
+  );
+}
+
+export default App
